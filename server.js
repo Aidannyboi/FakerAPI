bcddbbbc83a1fb83fca18814bd661dae97072501
Aidan Castellanos <133064388+Aidannyboi@users.noreply.github.com>
2023-11-18 15:29:08 -0800

    Added Company Creations route and functions.

--- server.js
+++ server.js
@@ -12,9 +12,37 @@ const createUser = () => ({
         _id: faker.string.uuid()
 });
 
+const createCompany = () => ({
+    _id: faker.string.uuid(),
+    name: faker.company.name(),
+    address: {
+        street: faker.location.street(),
+        city: faker.location.city(),
+        state: faker.location.zipCode(),
+        country: faker.location.country()
+    }
+});
+
 app.get("/api/user/new", (req, res) =>{
     let newUser = createUser();
     res.json(newUser);
+})
+
+app.get("/api/company/new", (req, res) =>{
+    let newCompany = createCompany();
+    res.json(newCompany);
+})
+
+app.get("/api/company/user", (req, res) =>{
+    let newCompany = createCompany();
+
+    let newUser = createUser();
+
+    const response = {
+        user: newUser,
+        company: newCompany
+    }
+    res.json(response);
 })
 
 app.listen(PORT, () => console.log("Server initiated on port" + PORT));
